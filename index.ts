cbc3942778b1a7c06900a187b6fc3a2c4fa8e33c
ahirota <avjhirota@gmail.com>
2026-01-16 12:06:44 -0800

    [TSD-3] Getting Ready to Demolish for Vite

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { log, chats } from "./chats";
 const button = document.getElementById("enable-button")!;
 
 button.addEventListener("click", () => {
+  // @ts-ignore
   window.supportAI.enableAutoReply();
 });
 
