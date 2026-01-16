428a668595579582917e9b2f7aa47fa45b6d3131
ahirota <avjhirota@gmail.com>
2026-01-16 12:09:25 -0800

    [TSD-4] Vite Project

--- index.js
+++
@@ -1,7 +0,0 @@
-import { log, chats } from "./chats";
-const button = document.getElementById("enable-button");
-button.addEventListener("click", () => {
-    // @ts-ignore
-    window.supportAI.enableAutoReply();
-});
-log(chats);
